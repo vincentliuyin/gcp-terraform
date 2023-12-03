dcc58d3d30084b6797bc759620b1b2c0b3e62a49
Vincent Liu <vincent.liu@windriver.com>
2023-12-03 01:52:43 -0800

    add manifest

---
+++ solutions/solution-security-enforce-multiple-app-GKE/python-flask-app/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import socket
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    hostname = socket.gethostname()
+    app_name = os.getenv('APP_NAME', "Default-app")  
+    web_content = os.getenv('WEB_CONTENT', "Empty")  
+    return f"<!DOCTYPE html><html><body><h1>This is APP: {app_name}</h1><h1>Hello from POD: {hostname}</h1>{web_content}</body></html>"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
